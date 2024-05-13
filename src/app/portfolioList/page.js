9675f287d0be340f3d75cf6cb45df79a82be17b4
Byeon Unseop <ghlejr316@naver.com>
2024-05-13 20:35:48 +0900

    next.js 폴더셋팅 1차

---
+++ src/app/portfolioList/page.js
@@ -0,0 +1,6 @@
+
+export default function PortfolioList() {
+    return (
+        <div>포트폴리오 리스트</div>
+    );
+}
---
+++ src/app/portfolioList/portfolioDetail/page.js
@@ -0,0 +1,6 @@
+
+export default function PortfolioDetail() {
+    return (
+        <div>포트폴리오 상세</div>
+    );
+}
