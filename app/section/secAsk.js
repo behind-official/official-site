61cc10312fa348d4dae58fbf073cbc7068221447
ghlejr316 <ghlejr316@naver.com>
2024-05-17 15:13:29 +0900

    secAsk파일에 DB에 데이터보내는 폼 적용

--- app/section/secAsk.js
+++ app/section/secAsk.js
@@ -1,8 +1,11 @@
 export default function Write() {
+
     return(
         <div>
             <h4>글작성</h4>
-            <form action="/api/ask" method="GET">
+            <form action="/api/post/new" method="POST">
+                <input name="title" placeholder="글제목"/>
+                <input name="content" placeholder="글내용"/>
                 <button type="submit">버튼</button>
             </form>
         </div>
