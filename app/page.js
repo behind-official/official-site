97d3a003fe99230b7f7092761dce7a9c7ada81b4
Dong Seung Shin <ttjh92@hanmail.net>
2024-05-18 21:27:00 +0900

    kv작업 및 폰트,이미지추가

--- app/page.js
+++ app/page.js
@@ -1,5 +1,8 @@
 import SecKv from './section/secKV.js'
 import SecAsk from './section/secAsk.js'
+import "./globals.css";
+import "./css/reset.css";
+import "./css/sec_kv.css";
 
 
 export default async function Home() {
