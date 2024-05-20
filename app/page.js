09f932a27b94af686310e8be6bf876e00592ddc4
ghlejr316 <ghlejr316@naver.com>
2024-05-20 18:22:55 +0900

    포트폴리오 css 추가

--- app/page.js
+++ app/page.js
@@ -1,14 +1,12 @@
 import SecKv from './section/secKV.js'
 import SecAsk from './section/secAsk.js'
-import "./globals.css";
-import "./css/reset.css";
 import "./css/sec_kv.css";
 
 
 export default async function Home() {
 
   return (
-    <main>
+    <main className='sec_project_wrap'>
       <SecKv/>
       <SecAsk/>
     </main>
