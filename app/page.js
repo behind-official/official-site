c0cf41bd6cabb3c930f499297214724a484a6740
Dongseung <ttjh92@hanmail.net>
2024-05-21 15:45:33 +0900

    타이틀 애미메이션 구현

--- app/page.js
+++ app/page.js
@@ -2,7 +2,6 @@ import SecKv from './section/secKV.js'
 import SecAsk from './section/secAsk.js'
 import "./globals.css";
 import "./css/reset.css";
-import "./css/sec_kv.css";
 
 
 export default async function Home() {
--- app/section/secKV.js
+++ app/section/secKV.js
@@ -1,11 +1,57 @@
+'use client';
+
+import { useEffect } from "react"
+import "../css/sec_kv.css";
+
+function prepareText() {
+    const textElements = document.querySelectorAll('.animated-text');
+    textElements.forEach(element => {
+        const animateLetters = element.textContent.split('');
+        element.textContent = '';
+        animateLetters.forEach((letter, i) => {
+            const span = document.createElement('span');
+            span.textContent = letter;
+            span.style.opacity = '0';
+            span.style.transform = 'translateY(100%)';
+            span.style.display = 'inline-block';
+            span.style.transition = 'opacity 0.5s ease, transform 0.5s ease';
+            element.appendChild(span);
+        });
+    });
+}
+
+function animateText() {
+    const textElements = document.querySelectorAll('.animated-text');
+    textElements.forEach((element, index) => {
+        element.style.opacity = '1';
+        element.style.visibility = 'visible';
+        const spanElements = element.querySelectorAll('span');
+        spanElements.forEach((span, i) => {
+            setTimeout(() => {
+                span.style.opacity = '1';
+                span.style.transform = 'translateY(0)';
+            }, i * 100);
+        });
+    });
+}
+
+
 export default function Kv() {
+    useEffect(() => {
+        prepareText();
+        animateText();
+    }, []);
+    
     return(
         <section className="sec_kv">
-            <div className="text_box">
-                <p>[<span className="text_padding01">BRINGING</span></p>
-                <p>VISIONARY<i className="text_icon"></i></p>
-                <p className="text_weight">To Reality</p>
-                <p><span className="text_padding02">YOUR IDEAS</span>]</p>
+            <div className="kv_container">
+                <div className="text_box">
+                    <p className="animated-text text_margin01">[BRINGING</p>
+                    <p className="animated-text">VISIONARY<i className="text_icon"></i></p>
+                    <p className="animated-text light_text">To Reality</p>
+                    <p className="animated-text description_text text_margin02">YOUR IDEAS]</p>
+                </div>
+
             </div>
         </section>
     )
