aa3ae1fa4995ba7254e7781c11c50d4541d8f61c
David <behind.lv@gmail.com>
2024-05-27 17:58:44 +0900

    [ING] ask 영역 css 작업중

--- app/section/secAsk.js
+++ app/section/secAsk.js
@@ -8,46 +8,51 @@ export default function Write() {
             <form action="/api/post/new" method="POST">
                 <ul className="ask_list">
                     <li className="ask_item">
-                        <label for="compayName">회사명 *</label>
-                        <input type="text" id="companyName" required/>
+                        <label className="label" for="compayName">회사명 *</label>
+                        <input type="text" placeholder="BEHIND" id="companyName" required/>
                     </li>
                     <li className="ask_item">
-                        <label for="managerName">담당자 *</label>
-                        <input type="text" id="managerName" required/>
+                        <label className="label" for="managerName">담당자 *</label>
+                        <input type="text" placeholder="Eastway Hong" id="managerName" required/>
                     </li>
                     <li className="ask_item">
-                        <label for="managerEmail">이메일 *</label>
-                        <input type="email" id="managerEmail" required/>
+                        <label className="label" for="managerEmail">이메일 *</label>
+                        <input type="email" placeholder="admin@behind.co.kr" id="managerEmail" required/>
                     </li>
                     <li className="ask_item">
-                        <label for="managerPhone">연락처 *</label>
-                        <input type="tel" id="managerPhone" required/>
+                        <label className="label" for="managerPhone">연락처 *</label>
+                        <input type="tel" placeholder="010-****-****" id="managerPhone" required/>
                     </li>
                     <li className="ask_item">
-                        <lable for="budget">예산 *</lable>
-                        <select id="budget">
-                            <option value="500">500만원 이하</option>
-                            <option value="400">400만원 이하</option>
-                            <option value="300">300만원 이하</option>
-                            <option value="200">200만원 이하</option>
-                            <option value="100">100만원 이하</option>
-                        </select>
+                        <div className="select_box">
+                            <label for="budget">예산 *</label>
+                            <select id="budget" className="select">
+                                <option disabled selected>예산을 선택해주세요.</option>
+                                <option value="500만원 이하">500만원 이하</option>
+                                <option value="400만원 이하">400만원 이하</option>
+                                <option value="300만원 이하">300만원 이하</option>
+                                <option value="200만원 이하">200만원 이하</option>
+                                <option value="100만원 이하">100만원 이하</option>
+                            </select>
+                        </div>
                     </li>
                     <li className="ask_item">
-                        <lable for="budget">서비스 종류 *</lable>
-                        <select id="budget">
-                            <option value="웹사이트">웹사이트</option>
-                            <option value="광고영상">광고영상</option>
-                            <option value="뮤직비디오">뮤직비디오</option>
-                        </select>
+                        <div className="select_box">
+                            <label for="budget">서비스 종류 *</label>
+                            <select id="budget" className="select">
+                                <option value="웹사이트">웹사이트</option>
+                                <option value="광고영상">광고영상</option>
+                                <option value="뮤직비디오">뮤직비디오</option>
+                            </select>
+                        </div>
                     </li>
                     <li className="ask_item">
-                        <lable for="askCont">문의내용</lable>
-                        <input type="text" id="askCont"/>
+                        <label for="askCont">문의내용</label>
+                        <input type="text" placeholder="내용을 입력하세요" id="askCont"/>
                     </li>
                 </ul>
                 
-                <button type="submit">버튼</button>
+                <button type="submit" className="btn_submit">SUBMIT</button>
             </form>
         </div>
     )
