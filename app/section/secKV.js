11879d609a9197a0f2bff880bd3497a771b03cfc
Dongseung <ttjh92@hanmail.net>
2024-08-25 19:30:22 +0900

    아이콘 애니메이션 추가

--- app/section/secKV.js
+++ app/section/secKV.js
@@ -10,16 +10,28 @@ gsap.registerPlugin(ScrollTrigger);
 function prepareText() {
     const textElements = document.querySelectorAll('.animated-text');
     textElements.forEach(element => {
-        const animateLetters = element.textContent.split('');
-        element.textContent = '';
-        animateLetters.forEach((letter) => {
-            const span = document.createElement('span');
-            span.textContent = letter;
-            span.style.opacity = '0';
-            span.style.transform = 'translateY(100%)';
-            span.style.display = 'inline-block';
-            span.style.transition = 'opacity 0.5s ease, transform 0.5s ease';
-            element.appendChild(span);
+        // 모든 자식 노드를 순회하면서 텍스트 노드만 <span>으로 감싸기
+        const childNodes = Array.from(element.childNodes);
+
+        element.innerHTML = ''; // 기존 내용을 초기화합니다.
+
+        childNodes.forEach(node => {
+            if (node.nodeType === Node.TEXT_NODE) {
+                // 텍스트 노드일 경우 각 문자별로 <span>으로 감싸기
+                const letters = node.textContent.split('');
+                letters.forEach(letter => {
+                    const span = document.createElement('span');
+                    span.textContent = letter;
+                    span.style.opacity = '0';
+                    span.style.transform = 'translateY(100%)';
+                    span.style.display = 'inline-block';
+                    span.style.transition = 'opacity 0.5s ease, transform 0.5s ease';
+                    element.appendChild(span);
+                });
+            } else {
+                // 텍스트 노드가 아닌 경우 (img 등)은 그대로 추가
+                element.appendChild(node);
+            }
         });
     });
 }
@@ -43,6 +55,13 @@ export default function Kv() {
     useEffect(() => {
         prepareText();
         animateText();
+
+        gsap.to(".text_icon", {
+            rotation: 360,
+            duration: 5,
+            repeat: -1, // 무한 반복
+            ease: "linear"
+        });
 
         gsap.set(".img_box", { y: 0 });
 
@@ -89,10 +108,10 @@ export default function Kv() {
 
     return (
         <section className="sec_kv">
-            <div className="kv_container">
+            <div className="kv_container inner_1520">
                 <div className="text_box animate_box">
                     <p className="animated-text text_margin">[BRINGING</p>
-                    <p className="animated-text">VISIONARY<i className="text_icon"></i></p>
+                    <p className="animated-text">VISIONARY<img src="./../images/kv/text_icon.png" alt="별표모양 아이콘" className="text_icon" /></p>
                     <p className="animated-text light_text">To Reality</p>
                     <p className="animated-text description_text text_padding">YOUR IDEAS]</p>
                 </div>
